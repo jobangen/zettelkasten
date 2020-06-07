23f2287b301470a24d185a7dff51f43f4b9a3c66
Jan Ole Bangen <jobangen@gmail.com>
2020-06-07 17:20:59 +0200

    add zettelkasten-cache

---
+++ zettelkasten-cache.el
@@ -0,0 +1,11 @@
+;;; zettelkasten-cache.el --- -*- lexical-binding: t -*-
+
+;;; Commentary:
+;;
+;; Zettelkasten-cache
+;;
+;;; Code:
+
+
+(provide 'zettelkasten-cache)
+;;; zettelkasten-cache.el ends here
