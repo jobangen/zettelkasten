b6fc111edd03e455a42f518ad1bffbdbeba0b85f
Jan Ole Bangen <jobangen@gmail.com>
2020-06-28 19:05:25 +0200

    fix zettelkasten-cache

--- zettelkasten-cache.el
+++ zettelkasten-cache.el
@@ -7,7 +7,7 @@
 ;;; Code:
 (require 'org-element)
 (require 'org-el-cache)
-
+(require 's)
 
 (defun zettelkasten-extract-value (element keyword)
   (org-element-map element 'keyword
@@ -16,7 +16,7 @@
           (org-element-property :value kw)))
     :first-match t))
 
-(defun zettelkasten-extract-title (element)
+(defun zettelkasten-extract-title (filename element)
   (zettelkasten-extract-value element "TITLE"))
 
 (defun zettelkasten-extract-id (filename)
@@ -30,7 +30,7 @@
            (s-chop-prefix "jr/" fname-chop))
           (t (s-left 15 fname-chop)))))
 
-(defun zettelkasten-extract-collections (element)
+(defun zettelkasten-extract-collections (filename element)
   (ignore-errors
     (let* ((collection-string
             (zettelkasten-extract-value element "COLLECTION"))
@@ -53,14 +53,21 @@
           (push descriptor collection-list)))
       collection-list)))
 
-(defun zettelkasten-extract-descriptors (element)
+(defun zettelkasten-extract-descriptors (filename element)
   (ignore-errors
     (let* ((descriptor-string
             (zettelkasten-extract-value element "DESCRIPTOR"))
            (descriptor-split
             (split-string descriptor-string))
+           (descriptor-headings
+            (-flatten (org-element-map element 'node-property
+               (lambda (property)
+                 (when (string= (org-element-property :key property) "DESCRIPTOR")
+                   (split-string (org-element-property :value property)))))))
+           (descriptor-conc
+            (append descriptor-split descriptor-headings))
            (descriptor-list nil))
-      (dolist (descriptor descriptor-split)
+      (dolist (descriptor descriptor-conc)
         (if (s-contains? zettelkasten-descriptor-chain-sep descriptor)
             (progn
               (let* ((chain-split
@@ -88,14 +95,59 @@
   (lambda (filename element)
     (list
      :file filename
-     :title (zettelkasten-extract-title element)
+     :title (zettelkasten-extract-title filename element)
      :id (zettelkasten-extract-id filename)
-     :collections (zettelkasten-extract-collections element)
+     :collections (zettelkasten-extract-collections filename element)
      :descriptors (zettelkasten-extract-descriptors filename element)
      :links (zettelkasten-extract-link-ids filename element))))
 
 ;; Update / Initialize the cache
 (add-hook 'after-save-hook (lambda () (org-el-cache-update zettelkasten-cache)))
+
+;;;###autoload
+(defun zettelkasten-force-update-cache ()
+  (interactive)
+  (org-el-cache-force-update zettelkasten-cache))
+
+(defun zettelkasten-cache-entry-filename (filename)
+  (org-el-cache-get zettelkasten-cache filename))
+
+(defun zettelkasten-cache-entry-id (id)
+  (car (org-el-cache-select
+        zettelkasten-cache
+        (lambda (filename entry)
+          (string= (plist-get entry :id) id)))))
+
+(defun zettelkasten-cache-entries-where-member (match key)
+  (org-el-cache-select
+   zettelkasten-cache
+   (lambda (filename entry)
+     (member match (plist-get entry key)))))
+
+(defun zettelkasten-cache-values-collection ()
+  (let ((collections nil))
+    (org-el-cache-each
+     zettelkasten-cache
+     (lambda (filename data)
+       (setq collections
+             (cons
+              (plist-get data :collections)
+              collections))))
+    (delete-dups (-flatten collections))))
+
+(defun zettelkasten-cache-values-descriptor (&optional entries)
+  (delete-dups
+   (-flatten
+    (if entries
+        (mapcar
+         (lambda (arg)
+           (plist-get arg :descriptors))
+         entries)
+      (org-el-cache-map
+       zettelkasten-cache
+       (lambda (filename entry)
+         (plist-get entry :descriptors)))))))
+
 
 (provide 'zettelkasten-cache)
 ;;; zettelkasten-cache.el ends here
