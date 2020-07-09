9a12a4f01a3d33130be0864a005f1f9b634df106
Jan Ole Bangen <jobangen@gmail.com>
2020-07-09 11:16:03 +0200

    fix zettelkasten-cache-entry-filename add optional

--- zettelkasten-cache.el
+++ zettelkasten-cache.el
@@ -109,14 +109,14 @@
   (interactive)
   (org-el-cache-force-update zettelkasten-cache))
 
-(defun zettelkasten-cache-entry-filename (filename)
-  (org-el-cache-get zettelkasten-cache filename))
-
 (defun zettelkasten-cache-entry-id (id)
   (car (org-el-cache-select
         zettelkasten-cache
         (lambda (filename entry)
           (string= (plist-get entry :id) id)))))
+(defun zettelkasten-cache-entry-filename (&optional filename)
+  (org-el-cache-get zettelkasten-cache (or filename (buffer-file-name))))
+
 
 (defun zettelkasten-cache-entries-where-member (match key)
   (org-el-cache-select
