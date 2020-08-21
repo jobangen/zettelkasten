b9f515acb0c3768fb768befe05603df451df5907
Jan Ole Bangen <jobangen@gmail.com>
2020-08-21 12:09:56 +0200

    add dir eph

--- zettelkasten-cache.el
+++ zettelkasten-cache.el
@@ -28,6 +28,8 @@
            (s-chop-prefix "txt/" fname-chop))
           ((s-prefix? "jr" fname-chop)
            (s-chop-prefix "jr/" fname-chop))
+          ((s-prefix? "eph" fname-chop)
+           (s-left 15 (s-chop-prefix "jr/" fname-chop)))
           (t (s-left 15 fname-chop)))))
 
 (defun zettelkasten-extract-collections (filename element)
